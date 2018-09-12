efd11e5211dfeb3cc5f69c96e9204956123a43e1
Lai Weng Han <jlwhoo7@gmail.com>
2018-09-12 17:23:37 +0800

    Added ora

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import commander from "commander";
 import chalk from "chalk";
+const ora = require("ora");
 
 import intergrityGen from "./integrityGen";
 
 commander
   .version("1.0.0")
-  .command("generate")
-  .alias("gen")
+  .command("generate <cdn_url>")
   .alias("g")
   .description("generate links based on url given")
   .action(async d => {
     if (d.length > 0) {
+      console.log(chalk.blue(`Generatring hashes for [${d}]`));
+      const spinner = ora("Loading...").start();
       try {
+        spinner.stop();
         const result = await intergrityGen(d);
         console.log(chalk.green(result));
       } catch (err) {
+        spinner.stop();
         console.error(chalk.red(`Invalid link for a CDN`));
       }
     } else {
